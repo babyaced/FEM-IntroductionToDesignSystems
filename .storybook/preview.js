c739418710e62326b1c08c0cd215d69b0d7edbf6
babyaced <dannyr.simpson@gmail.com>
2021-06-06 16:47:41 -0700

    added modal story and custom theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import {addParameters, addDecorator} from "@storybook/react";
 import { withContexts } from "@storybook/addon-contexts/react";
 import {contexts} from './contexts';
+import {withKnobs} from  "@storybook/addon-knobs"
+import {withA11y} from "@storybook/addon-a11y"
 
 addParameters({
   // backgrounds: [
@@ -10,3 +12,8 @@ addParameters({
 });
 
 addDecorator(withContexts(contexts));
+
+addDecorator(withKnobs);
+
+addDecorator(withA11y);
+
